f91d7267f17686ad31635cb52bb085ab978cecb6
Bartlomiej Zimny <b.zimny@getprintbox.com>
2020-08-05 23:43:08 +0200

    remove noise

--- index.js
+++ index.js
@@ -231,12 +231,10 @@ var dither = async function (imagePath, secondImagePath) {
 }
 
 var removeNoise = async function (imagePath, secondImagePath) {
-    const image1 = await Jimp.read(imagePath).color([{ apply: 'desaturate', params: [90] }])
-        .contrast(1);
-    const image2 = await (await Jimp.read(secondImagePath)).color([{ apply: 'desaturate', params: [90] }])
-        .contrast(1);
+    const image1 =  await (await (await Jimp.read(imagePath)).color([{ apply: 'desaturate', params: [90] }])).contrast(1);
+    const image2 =  await (await (await Jimp.read(secondImagePath)).color([{ apply: 'desaturate', params: [90] }])).contrast(1);
 
-    console.log(`[ Dither ]`);
+    console.log(`[ Remove Noise ]`);
     console.log(oneSquare(image1, image2));
     console.log(fiveSquare(image1, image2));
     console.log(bigSquare(image1, image2));
@@ -247,18 +245,17 @@ var removeNoise = async function (imagePath, secondImagePath) {
 
 
 var main = async function () {
-    for (var i = 1; i < 126; i++)
-        for (var j = 1; j < 126; j++) {
-            if (i != j) {
-                const image1 = `images/img${i}.jpeg`;
-                const image2 = `images/img${j}.jpeg`;
+    // for (var i = 1; i < 126; i++)
+    //     for (var j = 1; j < 126; j++) {
+    //         if (i != j) {
+    const image1 = `images/img21.jpeg`;
+    const image2 = `images/img4.jpeg`;
 
-                await greyScale(image1, image2);
-                await normalize(image1, image2);
-                await blur(image1, image2);
-                await gaussianBlur(image1, image2);
-            }
-        }
+    //             await normalize(image1, image2);
+    //             await blur(image1, image2);
+    await removeNoise(image1, image2);
+    //    }
+    //  }
 
 
 }
